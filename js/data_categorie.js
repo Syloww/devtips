ade5db6e656dc05e1a126b6c6a4f20a677563b6d
sylow <glenacsalim@gmail.com>
2024-09-17 10:53:38 +0200

    amélioration boutons

--- js/data_categorie.js
+++ js/data_categorie.js
@@ -3,30 +3,30 @@ const dataCategorie = [
     {
         id: "1",
         title: "Les cours HTML",
-        content0: "<h3><a href='detailCours.html?langage=html&numero=1'>Les bases HTML</a></h3>",
-        content1: "<h3><a href='detailCours.html?langage=html&numero=2'>Les HTML</a></h3>",
-        content2: "<h3><a href='detailCours.html?langage=html&numero=3'>Allez plus loins</a></h3>"
+        content0: "<h3><a href='detailCours.html?langage=html&numero=1' class='boutoncours'>Les bases HTML</a></h3>",
+        content1: "<h3><a href='detailCours.html?langage=html&numero=2' class='boutoncours'>Les HTML</a></h3>",
+        content2: "<h3><a href='detailCours.html?langage=html&numero=3' class='boutoncours'>Allez plus loins</a></h3>"
     },
     {
         id: "2",
         title: "Les cours CSS",
-        content0: "<h3><a href='detailCours.html?langage=css&numero=1'>Les bases CSS</a></h3>",
-        content1: "<h3><a href='detailCours.html?langage=css&numero=2'>Les CSS</a></h3>",
-        content2: "<h3><a href='detailCours.html?langage=css&numero=3'>CSS</a></h3>"
+        content0: "<h3><a href='detailCours.html?langage=css&numero=1' class='boutoncours'>Les bases CSS</a></h3>",
+        content1: "<h3><a href='detailCours.html?langage=css&numero=2' class='boutoncours'>Les CSS</a></h3>",
+        content2: "<h3><a href='detailCours.html?langage=css&numero=3' class='boutoncours'>CSS</a></h3>"
     },
     {
         id: "3",
         title: "Les cours JavaScript",
-        content0: "<h3><a href='detailCours.html?langage=js&numero=1'>Les bases JS</a><h3>",
-        content1: "<h3><a href='detailCours.html?langage=js&numero=2'>Le JS</a><h3>",
-        content2: "<h3><a href='detailCours.html?langage=js&numero=3'>JS</a><h3>"
+        content0: "<h3><a href='detailCours.html?langage=js&numero=1' class='boutoncours'>Les bases JS</a><h3>",
+        content1: "<h3><a href='detailCours.html?langage=js&numero=2' class='boutoncours'>Le JS</a><h3>",
+        content2: "<h3><a href='detailCours.html?langage=js&numero=3' class='boutoncours'>JS</a><h3>"
     },
     {
         id: "4",
         title: "Les cours PHP",
-        content0: "<h3><a href='detailCours.html?langage=php&numero=1'>Les bases PHP</a><h3>",
-        content1: "<h3><a href='detailCours.html?langage=php&numero=2'>Le PHP</a><h3>",
-        content2: "<h3><a href='detailCours.html?langage=php&numero=3'>PHP</a><h3>"
+        content0: "<h3><a href='detailCours.html?langage=php&numero=1' class='boutoncours'>Les bases PHP</a><h3>",
+        content1: "<h3><a href='detailCours.html?langage=php&numero=2' class='boutoncours'>Le PHP</a><h3>",
+        content2: "<h3><a href='detailCours.html?langage=php&numero=3' class='boutoncours'>PHP</a><h3>"
     }
 ];
 
