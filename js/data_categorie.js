06273dc4f1e0d30b2019197a3c62ace99e599a2f
arthurklr_ <arthur.k2104@gmail.com>
2024-10-08 14:08:54 +0200

    Update data_categorie.js

--- js/data_categorie.js
+++ js/data_categorie.js
@@ -5,7 +5,18 @@ const dataCategorie = [
         title: "Les cours HTML",
         content0: "<h3><a href='detailCours.html?langage=html&numero=1' class='boutoncours'>Chapitre 1 : Introduction à HTML</a></h3>",
         content1: "<h3><a href='detailCours.html?langage=html&numero=2' class='boutoncours'>Chapitre 2 : Structure de base d'une page HTML</a></h3>",
-        content2: "<h3><a href='detailCours.html?langage=html&numero=3' class='boutoncours'>Chapitre 3 : Textes et Titres</a></h3>"
+        content2: "<h3><a href='detailCours.html?langage=html&numero=3' class='boutoncours'>Chapitre 3 : Textes et Titres</a></h3>",
+        content3: "<h3><a href='detailCours.html?langage=html&numero=4' class='boutoncours'>Chapitre 4 : Listes</a></h3>",
+        content4: "<h3><a href='detailCours.html?langage=html&numero=5' class='boutoncours'>Chapitre 5 : Liens hypertextes</a></h3>",
+        content5: "<h3><a href='detailCours.html?langage=html&numero=6' class='boutoncours'>Chapitre 6 : Images</a></h3>",
+        content6: "<h3><a href='detailCours.html?langage=html&numero=7' class='boutoncours'>Chapitre 7 : Tableaux</a></h3>",
+        content7: "<h3><a href='detailCours.html?langage=html&numero=8' class='boutoncours'>Chapitre 8 : Formulaires</a></h3>",
+        content8: "<h3><a href='detailCours.html?langage=html&numero=9' class='boutoncours'>Chapitre 9 : Conteneurs et Sections</a></h3>",
+        content9: "<h3><a href='detailCours.html?langage=html&numero=10' class='boutoncours'>Chapitre 10 : Multimédia</a></h3>",
+        content10: "<h3><a href='detailCours.html?langage=html&numero=11' class='boutoncours'>Chapitre 11 : Iframes</a></h3>",
+        content11: "<h3><a href='detailCours.html?langage=html&numero=12' class='boutoncours'>Chapitre 12 : Attributs globaux</a></h3>",
+        content12: "<h3><a href='detailCours.html?langage=html&numero=13' class='boutoncours'>Chapitre 13 : Les métadonnées et SEO</a></h3>",
+        content13: "<h3><a href='detailCours.html?langage=html&numero=14' class='boutoncours'>Chapitre 14 : Accessibilité en HTML</a></h3>"
     },
     {
         id: "2",
